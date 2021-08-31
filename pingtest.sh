5069e15f865c9972f55d51f624ab485366bb32c1
ishanadadheech <ishana98dadhich@gmail.com>
2021-08-31 20:52:02 +0530

    Fourth Commit

--- pingtest.sh
+++ pingtest.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-FILE=errors.txt
-TARGET=192.168.0.1
+echo "Please enter IP: "
+read IP
+
+FILE=status.txt
+TARGET=$IP
 
 touch $FILE
 while true;
 do
 	DATE=$(date '+%d/%m/%Y %H:%M:%S')
-        ping -c 1 $TARGET &> /dev/null
+        ping -w 1 $TARGET &> /dev/null
             if [[ $? -ne 0 ]]; then
               echo "ERROR "$DATE
               echo $DATE >> $FILE
