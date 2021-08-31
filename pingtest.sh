5004e37642ab0fe3a4dfdd610545b65b93193eb5
ishanadadheech <ishana98dadhich@gmail.com>
2021-08-31 23:48:01 +0530

    Third Commit

--- pingtest.sh
+++ pingtest.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+#bash script which pings target every 5 seconds and logs output to a file.
 
+#Taking IP as input
 echo "Please enter IP: "
 read IP
 
-FILE=status.txt
+#Variable to store  IP  input
 TARGET=$IP
 
+#Creating file to monitor output
+FILE=status.txt
 touch $FILE
+
+#Condition check
 while true;
 do
 	DATE=$(date '+%d/%m/%Y %H:%M:%S')
-        ping -w 1 $TARGET &> /dev/null
+        ping -w 10 $TARGET >>$FILE
             if [[ $? -ne 0 ]]; then
               echo "ERROR "$DATE
-              echo $DATE >> $FILE
             else
               echo "OK "$DATE
             fi
